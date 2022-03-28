edaba46ceeb1e72747c87ac6999c222580b7c093
Maciej Chromik <chromik.maciej@gmail.com>
2022-03-28 22:46:21 +0200

    chapter 08 in progress

---
+++ 08tut/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: 0,
+      checked: false,
+      item: "One half pound bag of ...",
+    },
+    {
+      id: 1,
+      checked: true,
+      item: "Item 1",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    console.log(`key: ${id}`);
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  return (
+    <div className="App">
+      <Header title="Grocery List" />
+      <Content
+        items={items}
+        setItems={setItems}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
