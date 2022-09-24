5865d8588d072f0996cec8323441d3c1c17c35ca
Maciej Chromik <chromik.maciej@gmail.com>
2022-09-24 21:21:06 +0200

    15 complete

--- 15tut/src/App.js
+++ 15tut/src/App.js
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import Form from "./Form";
+// import List from "./List";
+import Table from "./Table";
 
 function App() {
   const API_URL = "https://jsonplaceholder.typicode.com/";
@@ -19,7 +22,13 @@ function App() {
     fetchItems();
   }, [reqType]);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form reqType={reqType} setReqType={setReqType} />
+      {/* <List items={items} /> */}
+      <Table items={items} />
+    </div>
+  );
 }
 
 export default App;
