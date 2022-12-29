b489de08a8d4a81b5f9a96b0cb67e56b6e09fe90
Maciej Chromik <chromik.maciej@gmail.com>
2022-12-30 00:16:48 +0100

    tut 16 complete

--- 16tut/src/App.js
+++ 16tut/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Header from "./Header";
-import Nav from "./Nav";
-import Footer from "./Footer";
+import Layout from "./Layout";
 import Home from "./Home";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
@@ -12,16 +10,34 @@ import { useState, useEffect } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <Home />
-      <NewPost />
-      <PostPage />
-      <About />
-      <Missing />
-      <Footer />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<Layout search={search} setSearch={setSearch} />}
+      >
+        <Route index element={<Home posts={searchResults} />} />
+        <Route path="post">
+          <Route
+            index
+            element={
+              <NewPost
+                handleSubmit={handleSubmit}
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+              />
+            }
+          />
+          <Route
+            path=":id"
+            element={<PostPage posts={posts} handleDelete={handleDelete} />}
+          />
+        </Route>
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
