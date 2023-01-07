01cd959f5204c6000f38e62987ae2fe6e3f9a5f5
Maciej Chromik <chromik.maciej@gmail.com>
2023-01-08 00:44:05 +0100

    up to 5:48:22 - searching not working propertly

--- 16tut/src/App.js
+++ 16tut/src/App.js
@@ -7,6 +7,7 @@ import About from "./About";
 import Missing from "./Missing";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { format } from "date-fns";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,7 +16,7 @@ function App() {
       title: "My First Post",
       datetime: "July 01, 2021 11:17:36 AM",
       body:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis con ...",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis con Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis con Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis con Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis con",
     },
     {
       id: 2,
@@ -41,6 +42,38 @@ function App() {
   ]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        post.body.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    );
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPost = [...posts, newPost];
+    setPosts(allPost);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
+
+  const handleDelete = (id) => {
+    const postList = posts.filter((post) => post.id !== id);
+    setPosts(postList);
+    navigate("/");
+  };
 
   return (
     <Routes>
@@ -48,47 +81,30 @@ function App() {
         path="/"
         element={<Layout search={search} setSearch={setSearch} />}
       >
-        <Route index element={<Home posts={posts} />} />
+        <Route index element={<Home posts={searchResults} />} />
         <Route path="post">
-          <Route index element={<NewPost />} />
-          <Route path=":id" element={<PostPage />} />
+          <Route
+            index
+            element={
+              <NewPost
+                handleSubmit={handleSubmit}
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+              />
+            }
+          />
+          <Route
+            path=":id"
+            element={<PostPage posts={posts} handleDelete={handleDelete} />}
+          />
         </Route>
         <Route path="about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Route>
     </Routes>
   );
-
-  // return (
-  //   <Routes>
-  //     <Route
-  //       path="/"
-  //       element={<Layout search={search} setSearch={setSearch} />}
-  //     >
-  //       <Route index element={<Home posts={searchResults} />} />
-  //       <Route path="post">
-  //         <Route
-  //           index
-  //           element={
-  //             <NewPost
-  //               handleSubmit={handleSubmit}
-  //               postTitle={postTitle}
-  //               setPostTitle={setPostTitle}
-  //               postBody={postBody}
-  //               setPostBody={setPostBody}
-  //             />
-  //           }
-  //         />
-  //         <Route
-  //           path=":id"
-  //           element={<PostPage posts={posts} handleDelete={handleDelete} />}
-  //         />
-  //       </Route>
-  //       <Route path="about" element={<About />} />
-  //       <Route path="*" element={<Missing />} />
-  //     </Route>
-  //   </Routes>
-  // );
 }
 
 export default App;
--- 16tut/src/Missing.js
+++ 16tut/src/Missing.js
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 import React from "react";
 const Missing = () => {
   return (
-    <main>
-      <h1>Missing</h1>
+    <main className="Missing">
+      <h2>Page Not Found</h2>
+      <p>Well, that's disappointing.</p>
+      <p>
+        <Link to="/">Visit Our Homepage</Link>
+      </p>
     </main>
   );
 };
