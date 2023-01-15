9c899212a517df911de454e348e3492d90336017
Maciej Chromik <chromik.maciej@gmail.com>
2023-01-15 21:27:43 +0100

    up to 6:55

---
+++ 20tut/src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+const About = () => {
+  return (
+    <main className="About">
+      <h1>About</h1>
+      <p style={{ marginTop: "1rem" }}>
+        This blog app is a project in the Learn React tutorial series.
+      </p>
+    </main>
+  );
+};
+
+export default About;
