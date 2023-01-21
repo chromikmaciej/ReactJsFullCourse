4921e1bd2948e797557afa4c94a46b772935f453
Maciej Chromik <chromik.maciej@gmail.com>
2023-01-21 15:37:54 +0100

    21tut complete

--- 21tut/src/Layout.js
+++ 21tut/src/Layout.js
@@ -7,8 +7,8 @@ import { Outlet } from "react-router-dom";
 const Layout = ({ search, setSearch, width }) => {
   return (
     <div className="App">
-      <Header title="React JS Blog" width={width} />
-      <Nav search={search} setSearch={setSearch} />
+      <Header title="React JS Blog" />
+      <Nav />
       <Outlet />
       <Footer />
     </div>
